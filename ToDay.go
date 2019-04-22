223930cf6ad01b67e6600a4974a562b7afb52f1b
李华峰 <lhf2008@yeah.net>
2019-04-22 19:05:39 +0800

    计算今天到周几还有几天
    计算今天到周几还有几天

---
+++ ToDay.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func toDay(day time.Weekday)  int{
+	today := time.Now().Weekday()
+	switch day {
+	case today+0:
+		return 0
+	case today+1:
+		return 1
+	case today+2:
+		return 2
+	case today+3:
+		return 3
+	case today+4:
+		return 4
+	case today+5:
+		return 5
+	case today+6:
+		return 6
+	default:
+		return 7
+	}
+}
+func main() {
+	fmt.Println(toDay(time.Saturday))
+}
